f5c8ad40ddebaf38a8ffdd0253fca9bbe5cc0cd8
muminoff <smuminov@gmail.com>
2016-02-08 06:51:51 +0000

    Completed: topic delete API

---
+++ api/topicdelete.js
@@ -0,0 +1,19 @@
+var topicDeleteQuery = "UPDATE topics SET archived=true WHERE id=$2 AND $2 IN (SELECT id FROM users WHERE roles ->> 'admin' = 'true')";
+
+var topicDelete = module.exports = function(client, user_id, topic_id, logger, callback) {
+
+  logger.debug('User ID inside api ->', user_id);
+
+  // topic create
+  client.query(topicDeleteQuery, [user_id, topic_id], function(err, result) {
+
+    if(err) {
+      logger.error(err);
+      return callback({ 'status': 'fail', 'detail': err });
+    }
+
+    return callback({ 'status': 'ok' });
+
+  });
+
+}
