8518fa414705ba1c46b0adf9303c1953cd61926a
muminoff <smuminov@gmail.com>
2016-02-26 05:27:49 +0000

    Fixed: topic members order by username

--- api/topicmembers.js
+++ api/topicmembers.js
@@ -1,4 +1,4 @@
-var getTopicMembersQuery = "select json_build_object('id', id, 'username', username, 'last_seen', (select (extract(epoch from last_seen) * 1000)::int8 from statuses where user_id=id)) as member, roles, profile, vt from users where id in (select user_id from subscribers where topic_id=$1)";
+var getTopicMembersQuery = "select json_build_object('id', id, 'username', username, 'last_seen', (select (extract(epoch from last_seen) * 1000)::int8 from statuses where user_id=id)) as member, roles, profile, vt from users where id in (select user_id from subscribers where topic_id=$1) order by username asc";
 
 var topicMembers = module.exports = function(client, topic_id, logger, callback) {
 
